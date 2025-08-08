f7feca697ad7921db5eba3b2472e884553c21833
LAPTOP-JRUMML2J\ronal <ronaldbejarano147>
2025-08-08 11:37:55 -0500

    obtener conexion

--- Controlador/ControllerConnection.cs
+++ Controlador/ControllerConnection.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
 
 namespace Controlador
 {
     public class ControllerConnection
     {
+        public static MySqlConnection getControllerConnection()
+        {
+            return Modelo.ModelConnection.getConnection();
+        }
     }
 }
+
